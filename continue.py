ed813814f6080d6e24d5dd0eef5681f1fd50c9a7
G-Ganesan <ganesan212001@gmail.com>
2023-04-03 11:43:28 +0530

    added 200,1000 to the cart

--- continue.py
+++ continue.py
@@ -7,7 +7,7 @@ Created on Sat Oct  3 19:51:09 2020
 #IF CONTINUE after continue tHat iterations won't be executed.will come to next loop.
 #if BREAK after break the loops will exit.won't going to execute the remaings
 #IF PASS
-cart=[100,20,5,200,600,800,25,15]
+cart=[100,20,5,200,600,800,25,15,300,1000]
 for items in cart:
     if items<=500:
         print("checked",items)
